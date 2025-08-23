e02b16bc8092e2bb76d3a046f4a22bf5dc345868
combo <laohang2024@gmail.com>
2025-08-23 23:15:38 +0800

    完善钱包地址创建

--- ruoyi-blockchain/src/main/java/com/ruoyi/blockchain/service/InnerBlockchainServer.java
+++ ruoyi-blockchain/src/main/java/com/ruoyi/blockchain/service/InnerBlockchainServer.java
@@ -5,6 +5,9 @@ import com.ruoyi.blockchain.chain.core.ChainType;
 import com.ruoyi.blockchain.chain.core.WalletModule;
 import com.ruoyi.blockchain.chain.wallet.WalletCreator;
 import com.ruoyi.blockchain.chain.wallet.WalletManager;
+import com.ruoyi.blockchain.domain.ChainBtcWallet;
+import com.ruoyi.blockchain.domain.ChainEthWallet;
+import com.ruoyi.blockchain.domain.ChainTronWallet;
 import com.ruoyi.blockchain.domain.MchChainWalletInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +25,15 @@ public class InnerBlockchainServer {
 
     @Resource
     IMchChainWalletInfoService mchChainWalletInfoService;
+
+    @Resource
+    IChainBtcWalletService chainBtcWalletService;
+
+    @Resource
+    IChainEthWalletService chainEthWalletService;
+
+    @Resource
+    IChainTronWalletService chainTronWalletService;
 
     @Value("${customParam.tokenFilePath}")
     private String tokenFilePath;
@@ -52,9 +65,33 @@ public class InnerBlockchainServer {
             mchChainWalletInfo.setCreatedTime(System.currentTimeMillis());
             bAccountInfoList.add(mchChainWalletInfo);
             mchChainWalletInfoService.insertMchChainWalletInfo(mchChainWalletInfo);
+
+            if(chainType.equals(ChainType.TRON)){
+                ChainTronWallet chainWallet = new ChainTronWallet();
+                chainWallet.setAddress(walletModule.getAddress());
+                chainWallet.setCreateTime(System.currentTimeMillis());
+                chainWallet.setUsdtBalance(BigDecimal.ZERO);
+                chainWallet.setTrxBalance(BigDecimal.ZERO);
+                chainTronWalletService.insertChainTronWallet(chainWallet);
+            }else if(chainType.equals(ChainType.ETH)){
+                ChainEthWallet chainWallet = new ChainEthWallet();
+                chainWallet.setAddress(walletModule.getAddress());
+                chainWallet.setCreateTime(System.currentTimeMillis());
+                chainWallet.setEthBalance(BigDecimal.ZERO);
+                chainEthWalletService.insertChainEthWallet(chainWallet);
+            }else if (chainType.equals(ChainType.BTC)){
+                ChainBtcWallet chainWallet = new ChainBtcWallet();
+                chainWallet.setAddress(walletModule.getAddress());
+                chainWallet.setCreateTime(System.currentTimeMillis());
+                chainWallet.setBtcBalance(BigDecimal.ZERO);
+                chainBtcWalletService.insertChainBtcWallet(chainWallet);
+            }
+
         }
         return bAccountInfoList;
     }
+
+
 
     public void monitorTron(){
         
